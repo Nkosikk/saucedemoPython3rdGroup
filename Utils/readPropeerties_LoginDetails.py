a0dbd50540ab1caf460c796df4e8560b9cb8ea40
nkosi <nkosi.cele@simplify.africa>
2024-10-15 18:49:07 +0200

    work allocation

--- Utils/readPropeerties_LoginDetails.py
+++ Utils/readPropeerties_LoginDetails.py
@@ -6,3 +6,9 @@ config.read("./Configurations/loginDetails.ini")
 class ReadLoginConfig():
     def getsauceDemoURL(self):
         return config.get("URLS","sauceDemoURL")
+
+
+
+
+    #TODO create function to read username =  MoreBlessing
+    #TODO create function to read password = Motlatso
