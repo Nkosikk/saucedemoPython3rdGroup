24561e4bbdfdf56188354d101551da34fdc4cf3b
nkosi <nkosi.cele@simplify.africa>
2024-10-15 19:41:20 +0200

    allocated work

--- Pages/loginPage.py
+++ Pages/loginPage.py
@@ -13,3 +13,9 @@ class LoginPage:
         wait = WebDriverWait(self.driver, 10)
         usernameElement = wait.until(EC.visibility_of_element_located((By.ID, self.textbox_username_id)))
         usernameElement.send_keys(username)
+
+        # ToDo create a code to enter password = Nelisiwe
+        # ToDo create a code to click login button = Igsaan
+        # ToDo create a code to verify that login is a success = Nhlanhla
+
+
